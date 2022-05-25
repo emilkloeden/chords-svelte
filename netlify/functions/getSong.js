4705a80e928db35ec1500193f594a3a7bc6e6ed8
Emil Kloeden <emilkloeden@gmail.com>
2022-05-25 21:33:41 +0930

    Yep

--- netlify/functions/getSong.js
+++ netlify/functions/getSong.js
@@ -5,14 +5,14 @@ export const handler = async (event, context) => {
     let { artist, title } = event.queryStringParameters;
     artist = artist.replace(".", "_")
     title = title.replace(".", "_")
-    const filePath = `./chords/${artist}/${title}.md`
-    const files = fs.readdirSync("./")
+    const filePath = `./netlify/functions/chords/${artist}/${title}.md`
+    const files = fs.readdirSync("./netlify/functions/chords/")
     return {
         statusCode: 200,
         body: JSON.stringify({
             message: "Test successful",
             path: path.resolve(process.cwd(), filePath),
-            // data: fs.readFileSync(filePath),
+            data: fs.readFileSync(filePath),
             files
         })
     }
